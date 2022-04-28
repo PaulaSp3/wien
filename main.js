cb09f3b59099d8fe6fd0c8ad3433f18ac6486be4
lwdtirol <lwd@tirol.gv.at>
2022-04-28 17:38:08 +0200

    mehrere Layer zur Auswahl hinzugefügt

--- main.js
+++ main.js
@@ -17,5 +17,11 @@ let map = L.map("map", {
 })
 
 let layerControl = L.control.layers({
-    "BasemapAT Grau":startLayer,
+    "BasemapAT Grau" : startLayer,
+    "BasemapAT Standard" : L.tileLayer.provider('BasemapAT.basemap'),
+    "BasemapAT Terrain" : L.tileLayer.provider('BasemapAT.terrain'),
+    "BasemapAT Surface" : L.tileLayer.provider('BasemapAT.surface'),
+    "BasemapAT High dpi" : L.tileLayer.provider('BasemapAT.highdpi'),
+    "BasemapAT Orthofoto" : L.tileLayer.provider('BasemapAT.orthofoto'),
+    "BasemapAT Overlay" : L.tileLayer.provider('BasemapAT.overlay'),
 }).addTo(map)
