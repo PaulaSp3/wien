33b7447f3e03ba8b5f22fb0bd8a3fa8a78dbdcf9
lwdtirol <lwd@tirol.gv.at>
2022-04-28 17:55:43 +0200

    Sehenswürdigkeiten am STtart anzeigen

--- main.js
+++ main.js
@@ -36,3 +36,5 @@ let sightLayer = L.featureGroup();
 layerControl.addOverlay(sightLayer, "Sehenswürdigkeiten");
 
 let mrk = L.marker([stephansdom.lat, stephansdom.lng]).addTo(sightLayer);
+
+sightLayer.addTo(map);
