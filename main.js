e1f365457ae458cd2dc81e1185b3eb4a4868a1f6
lwdtirol <lwd@tirol.gv.at>
2022-05-05 16:04:35 +0200

    asynchrone funktion zum Laden der GESOJSON datei mit Sehenswürdigkieten

--- main.js
+++ main.js
@@ -55,3 +55,11 @@ let miniMap = new L.Control.MiniMap(
     L.tileLayer.provider("BasemapAT"),
     {"toggleDisplay" : "True"}
 ).addTo(map);
+
+// Asynchrone Funktion
+async function loadSites(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    console.log(geojson);
+}
+loadSites("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json")
