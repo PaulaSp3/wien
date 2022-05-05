0eddd5e4676101012fe134b4d72450a60ed9665f
lwdtirol <lwd@tirol.gv.at>
2022-05-05 16:38:04 +0200

    Fußgängerzonen zu Overlay hinzugefügt

--- main.js
+++ main.js
@@ -101,3 +101,18 @@ async function loadLines(url) {
     L.geoJSON(geojson).addTo(overlay); // https://leafletjs.com/reference.html#geojson
 }
 loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json")
+
+//Fußgängerzonen
+async function loadZones(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log(geojson);
+
+    //Fußgängerzonen in Layercontrol
+    let overlay = L.featureGroup();
+
+    layerControl.addOverlay(overlay, "Fußgängerzonen");
+    overlay.addTo(map);
+    L.geoJSON(geojson).addTo(overlay); // https://leafletjs.com/reference.html#geojson
+}
+loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json")
