acfaabab107318889ef9a164d37676e2eaaa5c24
lwdtirol <lwd@tirol.gv.at>
2022-05-17 10:27:32 +0200

    Hotel suchen und auf Karte anzeigen

--- main.js
+++ main.js
@@ -252,12 +252,17 @@ async function loadHotels(url) {
 
     let form = document.querySelector("#searchForm");
     form.suchen.onclick = function() {
-        console.log(form.hotel.value);
+        
+        //console.log(form.hotel.value);
         hotelsLayer.eachLayer(function(marker) {
-            console.log(marker)
+            /*console.log(marker)
             console.log(marker.getLatLng())
             console.log(marker.getPopup())
-            console.log(marker.feature.properties.BETRIEB)
+            console.log(marker.feature.properties.BETRIEB)*/
+            if (form.hotel.value == marker.feature.properties.BETRIEB) {
+                map.setView(marker.getLatLng(), 17);
+                marker.openPopup();
+            }
         })
     }    
 
